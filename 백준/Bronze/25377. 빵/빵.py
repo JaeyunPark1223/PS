1a8cec0c2c59f61a4aeb3143eea31717cef9c6d7
JaeyunPark1223 <jaeyunpark28@gmail.com>
2024-06-17 18:41:22 +0900

    [Bronze IV] Title: 빵, Time: 116 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,9 +1,11 @@
+
 test = int(input())
 A = 1001
 for i in range(test):
   x, y = map(int,input().split())
   if x <= y and A>y:
-    A = y
-  else:
-    A = -1
+    A = y  
+
+if A == 1001: 
+  A = -1
 print(A)
