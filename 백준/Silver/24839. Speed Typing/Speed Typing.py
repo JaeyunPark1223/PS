b3777ccb9908e8ae264a0fee5384d6f58e6a3527
JaeyunPark1223 <jaeyunpark28@gmail.com>
2024-12-07 19:11:40 +0900

    [Silver IV] Title: Speed Typing, Time: 1528 ms, Memory: 35536 KB, Score: 5 point -BaekjoonHub

--- 백준/Silver/24839. Speed Typing/Speed Typing.py
+++ 백준/Silver/24839. Speed Typing/Speed Typing.py
@@ -7,9 +7,31 @@ for i in range(T):
   B = len(b)
   y = 0
   Y = a[0]
-  for j in range(B):
-    if b[j]==Y:
+  for j in range((A)-1):
+    if a[j] != a[j+1]:
       y+=1
-  if y >= A:
-    print("Case #"+str(i+1)+":",B-A)
-  else: print("Case #"+str(i+1)+":","IMPOSSIBLE")
+  if y == 0:
+    for j in range(B):
+      if b[j]==Y:
+        y+=1
+    if y >= A:
+      print("Case #"+str(i+1)+":",B-A)
+    else: print("Case #"+str(i+1)+":","IMPOSSIBLE")
+  else:
+    tmp = 0
+    for j in range(A):
+      x = 0
+      tmp1 = 0
+      for k in range(x, B):
+        if a[j] == b[k]:
+          b[k] = "#"
+          x = k
+          tmp+=1
+          tmp1+=1
+          break
+      if tmp1==0: 
+        print("Case #"+str(i+1)+":","IMPOSSIBLE")
+        break
+      elif tmp == A:
+        print("Case #"+str(i+1)+":",B-tmp)
+        break
