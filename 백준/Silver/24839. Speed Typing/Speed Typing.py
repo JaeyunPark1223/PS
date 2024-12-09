c0d272653dd8ffc58a9eb29978fd86dfa1384445
JaeyunPark1223 <jaeyunpark28@gmail.com>
2024-12-09 10:00:30 +0900

    [Silver IV] Title: Speed Typing, Time: 280 ms, Memory: 116376 KB, Score: 14 point -BaekjoonHub

--- 백준/Silver/24839. Speed Typing/Speed Typing.py
+++ 백준/Silver/24839. Speed Typing/Speed Typing.py
@@ -1,37 +1,30 @@
+# Optimized Code
 T = int(input())
 
-for i in range(T):
-  a = list(input())
-  b = list(input())
-  A = len(a)
-  B = len(b)
-  y = 0
-  Y = a[0]
-  for j in range((A)-1):
-    if a[j] != a[j+1]:
-      y+=1
-  if y == 0:
-    for j in range(B):
-      if b[j]==Y:
-        y+=1
-    if y >= A:
-      print("Case #"+str(i+1)+":",B-A)
-    else: print("Case #"+str(i+1)+":","IMPOSSIBLE")
-  else:
-    tmp = 0
-    for j in range(A):
-      x = 0
-      tmp1 = 0
-      for k in range(x, B):
-        if a[j] == b[k]:
-          b[k] = "#"
-          x = k
-          tmp+=1
-          tmp1+=1
-          break
-      if tmp1==0: 
-        print("Case #"+str(i+1)+":","IMPOSSIBLE")
-        break
-      elif tmp == A:
-        print("Case #"+str(i+1)+":",B-tmp)
-        break
+for case_number in range(1, T + 1):
+    a = input()
+    b = input()
+    A = len(a)
+    B = len(b)
+
+    # Check if all characters in 'a' are the same
+    if all(char == a[0] for char in a):
+        # Count occurrences of the same character in 'b'
+        count_in_b = b.count(a[0])
+        if count_in_b >= A:
+            print(f"Case #{case_number}: {B - A}")
+        else:
+            print(f"Case #{case_number}: IMPOSSIBLE")
+    else:
+        # Two pointers technique to match 'a' with 'b'
+        i = 0  # Pointer for 'a'
+        j = 0  # Pointer for 'b'
+        while i < A and j < B:
+            if a[i] == b[j]:
+                i += 1
+            j += 1
+        
+        if i == A:  # All characters in 'a' were matched
+            print(f"Case #{case_number}: {B - A}")
+        else:
+            print(f"Case #{case_number}: IMPOSSIBLE")
